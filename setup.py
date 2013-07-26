b9f161820ae881227702167b2ba33d478936df8d
Michael Mulich <michael.mulich@gmail.com>
2013-07-26 09:10:56 -0400

    Make it a package for better dependency resolution.

---
+++ setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+install_requires = (
+    )
+description = "An archive population utility."
+
+
+setup(
+    name='cnx-archive-population',
+    version='0.1',
+    author='Connexions team',
+    author_email='info@cnx.org',
+    url="https://github.com/connexions/cnx-archive-population",
+    license='LGPL, See aslo LICENSE.txt',
+    description=description,
+    packages=find_packages(),
+    install_requires=install_requires,
+    include_package_data=True,
+    entry_points="""\
+    [console_scripts]
+    cnx-archive-populate = populate:main
+    """,
+    # test_suite=''
+    )
